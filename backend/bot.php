dc1d35e5383c71067556c8eaf7ef5eddc6eac4b4
Ben Swierzy <swierzy@uni-bonn.de>
2021-09-19 22:52:19 +0200

    Bot fix

--- backend/bot.php
+++ backend/bot.php
@@ -16,33 +16,33 @@ try {
     $bot = new BotApi(TELEGRAM_BOT_TOKEN);
 
     $pdo = new PDO(sprintf("mysql:dbname=%s;host=%s", DB_NAME, DB_HOST), DB_USER, DB_PASS);
-    $stmt = $pdo->prepare('SELECT * FROM personalMedia WHERE date > CURRENT_TIMESTAMP - 3*3600');
+    $stmt = $pdo->prepare('SELECT * FROM personalMedia WHERE UNIX_TIMESTAMP(date) > UNIX_TIMESTAMP(CURRENT_TIMESTAMP) - 3*3600');
     $stmt->execute();
 
     /** @var $personalMedia PersonalMedia */
     while (($personalMedia = $stmt->fetchObject(PersonalMedia::class))) {
         foreach (TELEGRAM_CHAT_IDS as $chatId) {
-            $bot->sendMessage($chatId, "Neue Upload-Anfrage von $personalMedia->name: $personalMedia->request");
+            $bot->sendMessage($chatId, mb_convert_encoding("Neue Upload-Anfrage von $personalMedia->name: $personalMedia->request", "UTF-8"));
         }
     }
 
-    $stmt = $pdo->prepare('SELECT * FROM contact WHERE date > CURRENT_TIMESTAMP - 3*3600');
+    $stmt = $pdo->prepare('SELECT * FROM contact WHERE UNIX_TIMESTAMP(date) > UNIX_TIMESTAMP(CURRENT_TIMESTAMP) - 3*3600');
     $stmt->execute();
 
     /** @var $contact Contact */
     while (($contact = $stmt->fetchObject(Contact::class))) {
         foreach (TELEGRAM_CHAT_IDS as $chatId) {
-            $bot->sendMessage($chatId, "Neue Anfrage von $contact->name: $contact->request");
+            $bot->sendMessage($chatId, mb_convert_encoding("Neue Anfrage von $contact->name: $contact->request", "UTF-8"));
         }
     }
 
-    $stmt = $pdo->prepare('SELECT * FROM complaints WHERE date > CURRENT_TIMESTAMP - 3*3600');
+    $stmt = $pdo->prepare('SELECT * FROM complaints WHERE UNIX_TIMESTAMP(date) > UNIX_TIMESTAMP(CURRENT_TIMESTAMP) - 3*3600');
     $stmt->execute();
 
     /** @var $complaint Complaint */
     while (($complaint = $stmt->fetchObject(Complaint::class))) {
         foreach (TELEGRAM_CHAT_IDS as $chatId) {
-            $bot->sendMessage($chatId, "Neue Beschwerde von $complaint->name: $complaint->request");
+            $bot->sendMessage($chatId, mb_convert_encoding("Neue Beschwerde von $complaint->name: $complaint->request", "UTF-8"));
         }
     }
 
